f647e0c180a29465b5d04cef960ad9ccd943cb2f
gespok7773-afk <gespok7773@gmail.com>
2026-04-28 13:18:02 +0700

    3 этап

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
-require_once __DIR__ . '/Interfaces/UserRepositoryInterface.php';
-require_once __DIR__ . '/Repositories/JsonUserRepository.php';
-require_once __DIR__ . '/Repositories/MysqlUserRepository.php';
-require_once __DIR__ . '/Services/UserService.php';
-require_once __DIR__ . '/Controllers/HttpController.php';
-require_once __DIR__ . '/Controllers/ConsoleController.php';
-
-$chosen_db = parse_ini_file('.env');
-
-if ($chosen_db['DB_SOURCE'] === 'mysql') {
-    $db = new MysqlUserRepository($chosen_db);
-} elseif ($chosen_db['DB_SOURCE'] === 'json') {
-    $db = new JsonUserRepository();
-}
-
-$userService = new UserService($db);
-
-if (isset($_SERVER['HTTP_HOST'])) {
-    $controller = new HttpController($userService);
-}else{
-    $controller = new ConsoleController($userService);
-}
-$controller->request();
+require_once("app.php");
 
 
 
